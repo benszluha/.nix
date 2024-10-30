2fb16b579e1e753de2001de5e9849cac164a0d8a
benszluha <ben@szluha.co.za>
2024-10-30 09:48:05 +0200

    First Commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "WSL NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }; #home-manager
+  }; #Inputs
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+
+            users = {
+              nixos = import ./home-manager/nixos.nix;
+            }; #home-manager users
+          }; #home-manager module
+        } #home-manager
+      ]; #modules
+    }; #nixos host
+  }; #Outputs
+}
---
+++ home-manager/nixos.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, ... }:
+
+{
+    #Install home-manager and have it manage itself
+    programs.home-manager = {
+        enable = true;
+    }; #programs.home-manager
+
+    home.username = "nixos";
+    home.homeDirectory = "/home/nixos";
+
+    programs.zsh = {
+        enable = true;
+        oh-my-zsh = {
+            enable = true;
+            plugins = ["git"];
+            theme = "amuse";
+        }; #oh-my-zsh
+
+        shellAliases = {
+            nix_rebuild = "sudo nixos-rebuild switch --impure --flake ~/.nix";
+            win_host = "cd /mnt/c";
+        }; #Shell Aliases (zsh)
+
+        initExtra = ''
+        ${pkgs.neofetch}/bin/neofetch
+        '';
+    }; #zsh
+  
+    programs.git = {
+        enable = true;
+        userName = "benszluha";
+        userEmail = "ben@szluha.co.za";
+    }; #git
+
+    programs.ssh = {
+      enable = true;
+    }; #ssh
+
+
+    home.packages = with pkgs; [
+        neofetch
+
+        #utils
+        pciutils
+        usbutils
+        p7zip
+        zip
+        rar
+    ]; #home.packages
+
+    home.stateVersion = "24.05";
+}
