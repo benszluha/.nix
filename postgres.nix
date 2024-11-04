4868678cae5eb84813665de9961991723a0a1c81
benszluha <ben@szluha.co.za>
2024-11-04 08:47:12 +0200

    fixed syntax

--- postgres.nix
+++ postgres.nix
@@ -7,7 +7,7 @@
     #settings = {
     #  port = 4321;
     #};
-    authentication = pkgs.mkOverride 10 ''
+    authentication = pkgs.lib.mkOverride 10 ''
       #type database DBuser auth-method
       local all      all    trust
     '';
