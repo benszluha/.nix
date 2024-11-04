2a93799d7a48d29efaa32787953e279ab45f7aa5
benszluha <ben@szluha.co.za>
2024-11-04 08:52:16 +0200

    defined package

--- postgres.nix
+++ postgres.nix
@@ -1,12 +1,12 @@
+# postgres.nix
+# This is a minimal setup for postgresql this will be expanded on in future as needed.
+
 {lib, pkgs, ...}:
 
 {
   services.postgresql = {
     enable = true;
-    #package = "postgresql_16";
-    #settings = {
-    #  port = 4321;
-    #};
+    package = pkgs.postgresql_16;
     authentication = pkgs.lib.mkOverride 10 ''
       #type database DBuser auth-method
       local all      all    trust
