a22ef6388753317bb9acb6ff7b47958cdb80a975
benszluha <ben@szluha.co.za>
2024-11-07 14:42:14 +0200

    focal

---
+++ focalboard.nix
@@ -0,0 +1,10 @@
+systemd.services.focalboard = {
+  description = "Focalboard Container";
+  after = [ "network.target" ];
+  serviceConfig = {
+    ExecStart = "${pkgs.docker}/bin/docker start -a focal";
+    ExecStop = "${pkgs.docker}/bin/docker stop -t 2 focal";
+    Restart = "always";
+  };
+  wantedBy = [ "multi-user.target" ];
+};
