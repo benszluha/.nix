f7dd6142eecd20035c5d938dabe76a52d2ef6776
benszluha <ben@szluha.co.za>
2024-12-03 09:44:09 +0200

    Removed mongoDB

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        ./mongo.nix
+        #./mongo.nix
         
         home-manager.nixosModules.home-manager
         {
